802e7b5d0cf310762f8fa9056d8d1fb73a7bb580
Uday Panesar <uday.panesar@yash.com>
2020-01-02 18:07:32 +0530

    code uploaded

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModel;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        List<Customer> customer = new List<Customer>()
+            {
+                new Customer(){Name="John Smith",Id=1},
+                new Customer(){Name="Sher Khan",Id=2}
+            };
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            //var customer = new List<Customer>()
+            //{
+            //    new Customer(){Name="John Smith",Id=1},
+            //    new Customer(){Name="Sher Khan",Id=2}
+            //};
+
+            var customers = new CustomerViewModel()
+            {
+                Customers = customer
+            };
+            return View(customers);
+        }
+
+        
+        public ActionResult CustomerDetails(int id)
+        {
+            //if (customer.Contains() )
+            //{
+
+            //}
+            return View();
+        }
+    }
+}
---
+++ Vidly/Controllers/MovieController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModel;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        //public ActionResult Random()
+        //{
+        //    var movie = new Movie() { Name="Joker"};
+        //    var customers = new List<Customer>
+        //    {
+        //        new Customer() {Name="Customer1"},
+        //        new Customer() {Name="Customer2"}
+        //    };
+
+        //    var RandomMovie = new RandomMovieViewModel() { Movie = movie, Customers = customers };
+        //    return View(RandomMovie);
+        //}
+        //[Route("movie/edit/{id}/{name}")]
+        //public ActionResult edit(int? id, string name)
+        //{
+        //    return Content("id="+id+ "and name is"+name);
+        //}
+
+        public ActionResult Index() 
+        {
+            var movies = new List<Movie>
+            {
+                new Movie(){Name="Shrek"},
+                new Movie(){Name="Wall-E"}
+            };
+
+            var movieList = new MovieListViewModel() { Movies = movies };
+            return View(movieList);
+        }
+    }
+}
